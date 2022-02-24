27e606927e5c5f519d26889437d3ed0756aca58b
Bruno <bruno-mg300@hotmail.com>
2022-02-23 23:27:57 -0300

    first commit, setup the aplication

---
+++ src/containers/ProductComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductComponent = () => {
+    return (
+        <div className="four column wide">
+            <h1>ProductComponent</h1>
+        </div>
+    )
+}
+
+export default ProductComponent;
